fafad2d2cbdf10a1bf1181e92273d863619611e7
Justin Geiman <jgeiman@fireriskalliance.com>
2016-07-29 16:53:44 -0400

    modified sudo commands

--- 1-update-pkgs.sh
+++ 1-update-pkgs.sh
@@ -2,24 +2,18 @@
 
 # Install the packages needed for fireFoam
 
-# Use the root mode (sudo)
-sudo -s
-
 # update system
-apt-get update -y
+sudo apt-get update -y
 
 # Install packages for OpenFOAM and ParaView
-apt-get install -y git-core build-essential binutils-dev cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot \
+sudo apt-get install -y git-core build-essential binutils-dev cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot \
 libreadline-dev libncurses-dev libxt-dev libopenmpi-dev openmpi-bin libboost-system-dev libboost-thread-dev libgmp-dev \
 libmpfr-dev python python-dev libcgal-dev
 
 # Additional packages that may be needed for ParaView and CGAL
-apt-get install -y libglu1-mesa-dev libqt4-opengl-dev
+sudo apt-get install -y libglu1-mesa-dev libqt4-opengl-dev
 
 # Install additional packages for Scotch/PT-Scotch
-apt-get install -y libscotch-dev libptscotch-dev
-
-# exit root mode
-exit
+sudo apt-get install -y libscotch-dev libptscotch-dev
 
 
