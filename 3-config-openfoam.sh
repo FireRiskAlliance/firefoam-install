c0a091a3f382041fce496ed25024136043bde365
Justin Geiman <jgeiman@fireriskalliance.com>
2016-07-29 16:55:05 -0400

    corrected symbolic link paths

--- 3-config-openfoam.sh
+++ 3-config-openfoam.sh
@@ -13,7 +13,7 @@ fi
 echo "source $HOME/OpenFOAM/OpenFOAM-dev/etc/bashrc" >> $HOME/.bashrc
 
 # create symbolic links to avoid some MPI issues (may not be needed)
-ln -s /usr/bin/mpicc.openmpi OpenFOAM-dev/bin/mpicc
-ln -s /usr/bin/mpirun.openmpi OpenFOAM-dev/bin/mpirun
+ln -s /usr/bin/mpicc.openmpi $HOME/OpenFOAM/OpenFOAM-dev/bin/mpicc
+ln -s /usr/bin/mpirun.openmpi $HOME/OpenFOAM/OpenFOAM-dev/bin/mpirun
 
 
