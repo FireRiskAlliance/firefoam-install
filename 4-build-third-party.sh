3b2fd7531e09396b3a55310e7e801e4cfe5382c5
Justin Geiman <jgeiman@fireriskalliance.com>
2016-07-29 20:32:32 -0400

    added echo for paraview

--- 4-build-third-party.sh
+++ 4-build-third-party.sh
@@ -18,14 +18,17 @@ echo "building 3rd party software..."
 tail -n 10 log.make
 
 # Refresh shell environment
-wmRefresh
+source $HOME/.bashrc
 
 # change to ThirdParty folder
 cd $WM_THIRD_PARTY_DIR
  
 # make very certain the correct Qt version is used
 export QT_SELECT=qt4
- 
+
+echo
+echo "building ParaView ..." 
+
 # Build ParaView with python and mpi
 # This next command may take up to 2 hours
 if [ $(uname -m) = "i686" ]; then
