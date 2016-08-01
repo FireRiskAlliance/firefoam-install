eadbbfde55b77a73a1f5dd94c642a0f252f6c06a
Justin Geiman <jgeiman@fireriskalliance.com>
2016-08-01 07:33:40 -0400

    added command line argument option

--- change-run.sh
+++ change-run.sh
@@ -2,16 +2,26 @@
 
 echo "Current FOAM_RUN directory: $FOAM_RUN"
 
-echo -n "Enter the new FOAM_RUN path and press [ENTER]: "
-read RUN
-
-if [[ -z "$RUN" ]; then
-	exit 1
+if [[ -z "$1" ]]; then
+  # no directory provided, show prompt
+  echo -n "Enter the new FOAM_RUN path and press [ENTER]: "
+  read RUN
 else
-	# override FOAM_RUN env variable
-	echo "export FOAM_RUN=$RUN" >> $HOME/.bashrc
+  # use command line argument as new run directory
+  RUN="$1"
+  
+fi
 
-	# create the run directory, if needed
-	mkdir -p $FOAM_RUN
+if [[ -z "$RUN" ]]; then
+  # no directory provided
+  exit 1
+
+else
+  # override FOAM_RUN env variable
+  echo "export FOAM_RUN=$RUN" >> $HOME/.bashrc
+
+  # create the run directory, if needed
+  mkdir -pv "$RUN"
+
 fi
 
